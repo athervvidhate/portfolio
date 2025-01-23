d0495b57054722580316c77acadd8fb872cb88e7
Atherv Vidhate <58917647+athervvidhate@users.noreply.github.com>
2025-01-22 22:48:19 -0800

    added basic js for nav bar items

---
+++ global.js
@@ -0,0 +1,40 @@
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "resume/", title: "Resume" },
+  { url: "contact/", title: "Contact" },
+  { url: "https://github.com/athervvidhate", title: "Github" },
+  { url: "https://www.linkedin.com/in/athervvidhate/", title: "LinkedIn" },
+];
+
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+
+  url = !ARE_WE_HOME && !url.startsWith("http") ? "../" + url : url;
+
+  let title = p.title;
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+
+  a.classList.toggle(
+    'curr',
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  if (a.host !== location.host) {
+    a.target = "_blank"
+  }
+
+  
+}
