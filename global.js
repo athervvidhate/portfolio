eb9967efe86bdff651ee34f20d1c94b4de79de71
Atherv Vidhate <58917647+athervvidhate@users.noreply.github.com>
2025-01-30 21:25:42 -0800

    fixed projects on home page

--- global.js
+++ global.js
@@ -99,3 +99,9 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
   `;
   containerElement.appendChild(article);
 }
+
+export async function fetchGithubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+
+}
+
--- index.js
+++ index.js
@@ -4,5 +4,34 @@ const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
+const githubData = await fetchGithubData('athervvidhate');
+const profileStats = document.querySelector('#profile-stats');
 
-renderProjects(latestProjects, projectsContainer, 'h2');
+latestProjects.forEach(project => {
+    renderProjects(project, projectsContainer, 'h2');
+  });
+
+if (profileStats) {
+    profileStats.innerHTML = `
+          <h2 style="margin-top: 1rem;">My Github Stats</h2>
+          <dl class="stats-grid">
+            <div class="stat-item">
+              <dt>Followers</dt>
+              <dd>${githubData.followers}</dd>
+            </div>
+            <div class="stat-item">
+              <dt>Following</dt>
+              <dd>${githubData.following}</dd>
+            </div>
+            <div class="stat-item">
+              <dt>Public Repos</dt>
+              <dd>${githubData.public_repos}</dd>
+            </div>
+            <div class="stat-item">
+              <dt>Public Gists</dt>
+              <dd>${githubData.public_gists}</dd>
+            </div>
+          </dl>
+      `;
+  }
+  
